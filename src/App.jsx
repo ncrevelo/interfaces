c76d02f0629deb4cdb5db38c9c67b61cf3430128
“Camilo” <camilotello04@gmail.com>
2023-09-20 01:52:31 -0500

    cambio4

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,8 @@ export default function App() {
               <section className="  text-2xl font-oswald px-8 text-gray-700 flex-col justify-center text-justify width: 100px height: auto word-wrap: break-word order-1 pt-10" style={{ maxHeight: '200px', overflowY: 'auto' }}>
               <p > Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
-              
+              </section>
+              </section> 
           </section>
           
         </section>
