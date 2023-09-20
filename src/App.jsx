2b01066671345e4b10af82ab6526488889db9a85
“Camilo” <camilotello04@gmail.com>
2023-09-20 01:33:42 -0500

    cambios3

--- src/App.jsx
+++ src/App.jsx
@@ -44,10 +44,7 @@ export default function App() {
               <section className="  text-2xl font-oswald px-8 text-gray-700 flex-col justify-center text-justify width: 100px height: auto word-wrap: break-word order-1 pt-10" style={{ maxHeight: '200px', overflowY: 'auto' }}>
               <p > Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
-              </section>
-              <h2 className=" text-center text-3xl font-bold flex-col order-start" >Bienvenido</h2>
-              <h3 className=" flex flex-col order-2text-center justify-center font-oswald text-3xl">Soy Nathalia Cabrera</h3>
-              </section> 
+              
           </section>
           
         </section>
